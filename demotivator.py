eba172af8e9d1a311d705bdb5b741d9ff5dc599c
Dennis <dhkey14@icloud.com>
2024-07-26 10:02:40 +0200

    modified:   demotivator.py 	renamed:    times.ttf -> fonts/times.ttf 	renamed:    bg.png -> images/bg.png 	renamed:    image.png -> images/image.png 	renamed:    result.png -> images/result.png

--- demotivator.py
+++ demotivator.py
@@ -3,11 +3,11 @@ import os
 
 def make_meme(text, input, output="result.png", font_size=60):
 	image = input.resize((900,750))
-	image.save(f'temp.png')
-	bg = Image.open(f'bg.png')
+	image.save(f'images/temp.png')
+	bg = Image.open(f'images/bg.png')
 	width, height = bg.size
 	draw = ImageDraw.Draw(bg)
-	font = ImageFont.truetype('times.ttf', font_size)
+	font = ImageFont.truetype('fonts/times.ttf', font_size)
 	_, _, text_width, text_height = draw.textbbox((0, 0), text=text, font=font)
 	x = (width - text_width) / 2
 	y = (height - text_height)-100
@@ -17,14 +17,14 @@ def make_meme(text, input, output="result.png", font_size=60):
 		...
 
 	draw.text((x, y), text, font=font, fill=text_color)	
-	second = Image.open(f'temp.png')
+	second = Image.open(f'images/temp.png')
 	bg.paste(second, (96, 35))
 	bg.save(output)
 	bg.close()
 	second.close()
 	image.close()
-	os.remove("temp.png")
+	os.remove("images/temp.png")
 		
 
 
-make_meme("when roach dont want to jump", Image.open(f'image.png'), "result.png", 70)
+make_meme("when roach dont want to jump", Image.open(f'images/image.png'), "result.png", 70)
